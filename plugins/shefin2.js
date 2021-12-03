44c91c54dfdc97aa6cde154c8eee82d1158b8e08
Shefin-Sir <95488528+Shefin-Sir@users.noreply.github.com>
2021-12-03 21:05:36 +0530

    Fix

--- plugins/shefin2.js
+++ plugins/shefin2.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Amalser.
+/* Copyright (C) 2020 Shefin-Sir.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Amalser - Amal,farhan
+Shefin-Sir - Shefin,farhan
 */
 
 const Asena = require('../events');
